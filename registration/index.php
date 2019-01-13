289ba843947aab2d73d9ac26a000a4cfc4b17da7
Miles Oldenburg <milesoldenburg@gmail.com>
2019-01-13 13:14:27 -0500

    Removed link to 40th Reunion Registration Form

--- registration/index.php
+++ registration/index.php
@@ -3,15 +3,18 @@
 <h1>Registration</h1>
 <br />
 
-All Classmates are encouraged to complete and return a Registration Form.  Even if you cannot attend the Reunion Party, we would like to hear from you in order to update our records.
+All Classmates are encouraged to complete and return a Registration Form. Even if you cannot attend the Reunion Party, we would like to hear from you in order to update our records.
 <br /><br />
 
-<a href="registrationForm.pdf" title="Registration Form [PDF]." style="border: 1px #CCC solid; display: block; line-height: 30px; padding: 10px; text-align: center; width: 300px;">Download the Registration Form [PDF]</a>
-<br />
+The 45th Reunion Registration Form will be coming soon.
+<br/><br/>
 
-The Reunion Committee has tried to keep the pricing as low as possible.  The price includes the cost of your appetizers, other catering fees, the rental of the facility, and the postage and printing costs for mailings. Due to the event being catered, we would like to receive all registrations by July 10th.
+<!--<a href="registrationForm.pdf" title="Registration Form [PDF]." style="border: 1px #CCC solid; display: block; line-height: 30px; padding: 10px; text-align: center; width: 300px;">Download the Registration Form [PDF]</a>-->
+<!--<br />-->
+
+The Reunion Committee has tried to keep the pricing as low as possible. The price includes the cost of your appetizers, other catering fees, the rental of the facility, and the postage and printing costs for mailings. Due to the event being catered, we would like to receive all registrations by July 10th.
 <br /><br />
 
-<span style="font-size: 80%;"><em>[PDF] - Download <a href="http://get.adobe.com/reader/" title="Download Adobe Acrobat Reader.">Adobe Acrobat Reader</a></em></span>
+<!--<span style="font-size: 80%;"><em>[PDF] - Download <a href="http://get.adobe.com/reader/" title="Download Adobe Acrobat Reader.">Adobe Acrobat Reader</a></em></span>-->
 
 <?php include("../include/footer.php"); ?>
